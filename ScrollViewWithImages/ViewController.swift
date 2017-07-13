876a964839c2aba67cfcbd5120e2deb80f3c2f48
Niharika Sharma <niharikagarg40@gmail.com>
2017-07-12 20:29:49 -0400

    no message

--- ScrollViewWithImages/ViewController.swift
+++ ScrollViewWithImages/ViewController.swift
@@ -705,3 +705,27 @@ extension UIView {
     }
 }
 
+extension ViewController {
+    private func readJson() {
+        do {
+            if let file = Bundle.main.url(forResource: "points", withExtension: "json") {
+                let data = try Data(contentsOf: file)
+                let json = try JSONSerialization.jsonObject(with: data, options: [])
+                if let object = json as? [String: Any] {
+                    // json is a dictionary
+                    print(object)
+                } else if let object = json as? [Any] {
+                    // json is an array
+                    print(object)
+                } else {
+                    print("JSON is invalid")
+                }
+            } else {
+                print("no file")
+            }
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+}
+
